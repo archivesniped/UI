feee72ca038ab9573f1ed701be88535c0deab49a
archivesniped <vxarchived@gmail.com>
2025-05-06 15:53:32 -0400

    Update CustomConfigs.lua

--- CustomConfigs.lua
+++ CustomConfigs.lua
@@ -1,7 +1,7 @@
 return {
     ["fedarel"] = {
         hwid = "30373736344137382D324242422D353139352D424132392D433032423735433042363343",
-        clientid = "D3486AE9136E7856BC42212385EA797094475802"
+        clientid = "nil"
     },
     ["test"] = {
         hwid = "executor_hwid_here",
