bcd80aec2aaf1c6f3b286ab3329fd20f7a733303
archivesniped <vxarchived@gmail.com>
2025-05-06 22:10:42 -0400

    Update CustomConfigs.lua

--- CustomConfigs.lua
+++ CustomConfigs.lua
@@ -7,9 +7,9 @@ return {
         hwid = "nil",
         clientid = "1E77A517-FB06-4285-B48B-78202BCE908E"
     },
-    ["test"] = {
+    ["drynfthh"] = {
         hwid = "nil",
-        clientid = "test"
+        clientid = "51998c7d-3d0e-48c6-8d4b-0f7ee7e8b25e"
     },
     ["test"] = {
         hwid = "nil",
