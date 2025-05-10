7dbad2a7ed0b90b7ea1c3b581c298d4a1be9b3fb
archivesniped <vxarchived@gmail.com>
2025-05-10 19:54:25 -0400

    Update CustomConfigs.lua

--- CustomConfigs.lua
+++ CustomConfigs.lua
@@ -3,9 +3,9 @@ return {
         hwid = "30373736344137382D324242422D353139352D424132392D433032423735433042363343",
         clientid = "nil"
     },
-    ["thehyperborean_00"] = { -- expires 5/9/25
+    ["test"] = {
         hwid = "nil",
-        clientid = "1E77A517-FB06-4285-B48B-78202BCE908E"
+        clientid = "nil"
     },
     ["drynfthh"] = {
         hwid = "nil",
